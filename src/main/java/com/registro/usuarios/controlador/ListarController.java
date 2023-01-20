f1fbcb14380d169a9f4eb1820cb35bbbab90f658
auxprog <jonathanjjlbb1@gmail.com>
2023-01-20 13:42:21 -0400

    ver carrito y validacion

--- src/main/java/com/registro/usuarios/controlador/ListarController.java
+++ src/main/java/com/registro/usuarios/controlador/ListarController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -126,6 +128,16 @@ public class ListarController {
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
 
+		return "Productos/carrito";
+	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+	
 		return "Productos/carrito";
 	}
 	
